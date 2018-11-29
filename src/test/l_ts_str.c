e570162cf1b2345f350d0dd870e26dd971882ad8
tanx <qpoo@foxmail.com>
2018-11-30 04:48:41 +0800

    add l_strncmp_cap test

--- src/test/l_ts_str.c
+++ src/test/l_ts_str.c
@@ -4,10 +4,10 @@
 static void ts_find_str( void )
 {
 	char * p;
-	
+
 	string_t string = string("12345");
 	string_t find = string("34");
-	
+
 	p = l_find_str( string.data, string.len, find.data, find.len );
 	t_assert( p );
 	t_assert( *p == '3' );
@@ -17,9 +17,9 @@ static void ts_atoi( void )
 {
 	status rc;
 	int32 num;
-	
+
 	string_t str = string("12345");
-	
+
 	rc = l_atoi( str.data, str.len, &num );
 	t_assert( rc == OK );
 	t_assert( num == 12345 );
@@ -29,9 +29,9 @@ static void ts_atoi_minus( void )
 {
 	status rc;
 	int32 num;
-	
+
 	string_t str = string("-12345");
-	
+
 	rc = l_atoi( str.data, str.len, &num );
 	t_assert( rc == OK );
 	t_assert( num == -12345 );
@@ -41,9 +41,9 @@ static void ts_atoi_error( void )
 {
 	status rc;
 	int32 num;
-	
+
 	string_t str = string("0.45");
-	
+
 	rc = l_atoi( str.data, str.len, &num );
 	t_assert( rc == ERROR );
 }
@@ -52,9 +52,9 @@ static void ts_atof( void )
 {
 	status rc;
 	float num;
-	
+
 	string_t str = string("0.1234");
-	
+
 	rc = l_atof( str.data, str.len, &num );
 	t_assert( rc == OK );
 	t_assert(  num == (float)0.1234 );
@@ -64,9 +64,9 @@ static void ts_atof_minus( void )
 {
 	status rc;
 	float num;
-	
+
 	string_t str = string("-0.1234");
-	
+
 	rc = l_atof( str.data, str.len, &num );
 	t_assert( rc == OK );
 	t_assert(  num == (float)-0.1234 );
@@ -76,9 +76,9 @@ static void ts_atof_error( void )
 {
 	status rc;
 	float num;
-	
+
 	string_t str = string("-0.12x34");
-	
+
 	rc = l_atof( str.data, str.len, &num );
 	t_assert( rc == ERROR );
 }
@@ -87,9 +87,9 @@ static void ts_hex2dec( void )
 {
 	status rc;
 	int32 num;
-	
+
 	string_t str = string("0Xfff0");
-	
+
 	rc = l_hex2dec( str.data, str.len, &num );
 	t_assert( rc == OK );
 	t_assert( num == 65520 );
@@ -99,9 +99,9 @@ static void ts_hex2dec_minus( void )
 {
 	status rc;
 	int32 num;
-	
+
 	string_t str = string("-0Xfff0");
-	
+
 	rc = l_hex2dec( str.data, str.len, &num );
 	t_assert( rc == OK );
 	t_assert( num == -65520 );
@@ -111,26 +111,36 @@ static void ts_hex2dec_error( void )
 {
 	status rc;
 	int32 num;
-	
+
 	string_t str = string("-0Xfff#$0");
-	
+
 	rc = l_hex2dec( str.data, str.len, &num );
 	t_assert( rc != OK );
 }
+// -----------
+static void ts_strncmp_cap ( void )
+{
+	status rc;
+
+	rc = l_strncmp_cap( "ABCD1", l_strlen("ABCD1") ,"abcd1", l_strlen("abcd1") );
+	t_assert( rc == OK );
+}
 // --------------
-void ts_str_init(  ) 
+void ts_str_init(  )
 {
 	test_add( ts_find_str );
-	
+
 	test_add( ts_atoi );
 	test_add( ts_atoi_minus );
 	test_add( ts_atoi_error );
-	
+
 	test_add( ts_atof );
 	test_add( ts_atof_minus );
 	test_add( ts_atof_error );
-	
+
 	test_add( ts_hex2dec );
 	test_add( ts_hex2dec_minus );
 	test_add( ts_hex2dec_error );
-} 
+
+	test_add( ts_strncmp_cap );
+}
